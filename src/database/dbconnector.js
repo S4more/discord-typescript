1143b47c4bde0b131052bca4d5856a15ca68500d
S4more <guimsc@hotmail.com>
2021-01-28 22:56:29 +0000

    remote .js files

--- src/database/dbconnector.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pg_1 = require("pg");
-exports.default = new pg_1.Pool({
-    max: 20,
-    connectionString: 'postgres://pi:86491300@192.168.2.200:5432/dawson',
-    idleTimeoutMillis: 30000
-});
